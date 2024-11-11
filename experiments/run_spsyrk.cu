31bcb5792c3f43086b14079cb1af8dc1e6f3fd88
Hooninator <jbellavita@berkeley.edu>
2024-11-11 02:52:29 -0800

    update experiment

--- experiments/run_spsyrk.cu
+++ experiments/run_spsyrk.cu
@@ -6,7 +6,7 @@ using namespace symmetria;
 typedef unsigned int IT;
 typedef double DT;
 
-void run_spsyrk_1d(ExperimentConfig& config)
+void run_spsyrk_2d(ExperimentConfig& config)
 {
 
     symmetria_init();
@@ -18,10 +18,20 @@ void run_spsyrk_1d(ExperimentConfig& config)
         unsigned int n = config.cols;
         unsigned int nnz = config.nnz;
         unsigned int ntrials = config.ntrials;
-        std::shared_ptr<ProcMap> proc_map = std::make_shared<ProcMap>(symmetria::n_pes, MPI_COMM_WORLD);
+
+        const uint32_t mb = std::floor((float)m / (2*(int)sqrt(n_pes)));
+        const uint32_t nb = std::floor((float)n / (2*(int)sqrt(n_pes)));
+        const uint32_t mtiles = std::floor( (float)m / (float)mb );
+        const uint32_t ntiles = std::floor( (float)n / (float)nb );
+
+        std::shared_ptr<ProcMapCyclic2D> proc_map = std::make_shared<ProcMapCyclic2D>(
+                (int)sqrt(symmetria::n_pes), 
+                (int)sqrt(symmetria::n_pes), 
+                mtiles, ntiles,
+                MPI_COMM_WORLD);
 
         /* Read in matrix */
-        DistSpMat1DBlockRow<IT, DT> A(m, n, nnz, proc_map);
+        DistSpMatCyclic2D<IT, DT, ProcMapCyclic2D> A(m, n, nnz, mb, nb, proc_map);
         symmetria::io::read_mm<IT, DT>(path.c_str(), A);
         MPI_Barrier(MPI_COMM_WORLD);
 
@@ -31,7 +41,7 @@ void run_spsyrk_1d(ExperimentConfig& config)
         for (int i=0; i<ntrials; i++) 
         {
             timer_ptr->start_timer("SpSYRK");
-            auto C_computed = spsyrk_bulksync_1d_rowblock<Semiring>(A);
+            auto C_computed = spsyrk_cyclic_2d<Semiring>(A);
             timer_ptr->stop_timer("SpSYRK");
             double t = timer_ptr->get_timer("SpSYRK");
             
@@ -39,7 +49,7 @@ void run_spsyrk_1d(ExperimentConfig& config)
                 std::cout<<"Time for SpSYRK: "<<t<<"s"<<std::endl;
 
             /* Write timer to CSV */
-            std::string csv_name("timings_spsyrk_1d_"+STR(symmetria::my_pe)+".csv");
+            std::string csv_name("timings_spsyrk_2d_"+STR(symmetria::my_pe)+".csv");
 
             /* If first trial, clear the file, else append */
             if (i==0)
@@ -62,8 +72,8 @@ int main(int argc, char ** argv)
     ExperimentConfig config = parse_args(argc, argv);
     std::string type(config.type);
 
-    if (type.compare("1d")==0) {
-        run_spsyrk_1d(config);
+    if (type.compare("2d")==0) {
+        run_spsyrk_2d(config);
     }
 
     return 0;
